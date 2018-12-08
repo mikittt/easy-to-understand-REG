7b5d7f7f682396359bba1e7b40399e5c30be4afe
mikittt <tsudanumahyper@gmail.com>
2018-12-08 15:04:32 +0900

    add lines

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 - Results	from RefCOCOg
 
-<img src="https://raw.githubusercontent.com/mikittt/Human_Friendly_REG/master/demo/fig2.png" width="300">
+<img src="https://raw.githubusercontent.com/mikittt/Human_Friendly_REG/master/demo/fig2.png" width="50%">
 
 This code is implemented in python3 with [Chainer](https://github.com/chainer/chainer).
 
@@ -14,6 +14,29 @@ This code is implemented in python3 with [Chainer](https://github.com/chainer/ch
 ## Dataset
 
 Please go to [this directory](https://github.com/mikittt/Human_Friendly_REG/tree/master/pyutils/refer2/)
+
+## Preprocessing
+
+```
+python prepro.py --dataset refgta --splitBy utokyo
+```
+
+## Extract features
+- extract local features
+(we resize images to different sizes depending on their aspect ratio, so please set batch size to 1 for extracting local spatial features)
+
+```bash
+python scripts/extract_target_emb_feats.py --dataset refgta --splitBy utokyo --batch_sizze 64
+
+# local spatial features
+python scripts/extract_target_sp_feats.py --dataset refgta --splitBy utokyo --batch_size 1
+```
+
+- extract global features
+```bash
+python scripts/extract_image_sp_feats.py --dataset refgta --splitBy utokyo --batch_size 64
+```
+
 
 ### Acknowledgement
 Our codes are based on [this repositry](https://github.com/lichengunc/speaker_listener_reinforcer).
---
+++ config.py
@@ -0,0 +1,80 @@
+import argparse
+
+def parse_opt():
+	parser = argparse.ArgumentParser()
+	
+	parser.add_argument('--data_root', type=str, default='',
+						help='data folder containing four datasets.')
+	parser.add_argument('--coco_image_root', type=str, default='',
+						help='data folder containing MSCOCO images')
+	parser.add_argument('--gta_image_root', type=str, default='',
+						help='data folder containing gta images')
+	parser.add_argument('--save_dir', default='',
+						help='save directory')
+    
+	parser.add_argument('--data_json', default='data.json', help='converted json file')
+	parser.add_argument('--data_h5', default='data.h5', help='converted h5 file')
+	parser.add_argument('--old', '-old', action='store_true', help='the each test set of RefCOCO and RefCOCO+ have 2 versions')
+	# options
+	parser.add_argument('--dataset', '-d', default='refcoco', type=str, help='refcoco/refcoco+/refcocog/refgta')
+	parser.add_argument('--splitBy', '-s', default='unc', type=str, help='unc/google/utokyo')
+	
+	# preprocess options
+	parser.add_argument('--max_length', type=int, help='max length of a caption')  # refcoco 10, refclef 10, refcocog 20, refgta 20
+	parser.add_argument('--word_count_threshold', default=5, type=int,
+						help='only words that occur more than this number of times will be put in vocab')
+	# image feature extraction
+	parser.add_argument('--image_feats', default='sp_image_feats.npy', help='image, Variable feats file')
+	parser.add_argument('--sp_ann_feats', default='sp_ann_feats.npy', help='spatial ann feats file')
+	parser.add_argument('--ann_feats', default='ann_feats.npy', help='ann feats file')
+	parser.add_argument('--ann_shapes', default='shapes.npy', help='ann shapes file')
+
+	# pretraining settings
+	parser.add_argument('--sample_ratio', type=float, default=0.5)
+	parser.add_argument('--sample_neg', type=int, default=1)
+	parser.add_argument('--grad_clip', type=float, default=0.1)
+	parser.add_argument('--ranking_lambda', type=float, default=0.1)
+	parser.add_argument('--seq_per_ref', type=int, default=3)
+	parser.add_argument('--learning_rate_decay_start', type=int, default=8000)
+	parser.add_argument('--learning_rate_decay_every', type=int, default=8000)
+	parser.add_argument('--optim_epsilon', type=float, default=1e-8)
+	parser.add_argument('--losses_log_every', type=int, default=25)
+	parser.add_argument('--max_iter', type=int, default=-1)
+	parser.add_argument('--save_checkpoint_every', type=int, default=2000)
+	## language encoder
+	parser.add_argument('--learning_rate', type=float, default=4e-4)
+	parser.add_argument('--optim_alpha', type=float, default=0.8)
+	parser.add_argument('--optim_beta', type=float, default=0.999)
+	## visual encoder
+	parser.add_argument('--ve_learning_rate', type=float, default=4e-5)
+	parser.add_argument('--ve_optim_alpha', type=float, default=0.8)
+	parser.add_argument('--ve_optim_beta', type=float, default=0.999)
+	
+	parser.add_argument('--pretrained_w', '-pw', action='store_true')
+	parser.add_argument('--word_emb_path', default='word_emb.npy')
+
+	# training settings
+	parser.add_argument('--generation_weight', type=float, default=1)
+	parser.add_argument('--vis_rank_weight', type=float, default=1)
+	parser.add_argument('--lang_rank_weight', type=float, default=0)
+	parser.add_argument('--embedding_weight', type=float, default=1)
+	parser.add_argument('--rank_lam', type=float, default=0.4)
+	parser.add_argument('--lm_margin', type=float, default=1)
+	parser.add_argument('--emb_margin', type=float, default=0.1)
+	
+	parser.add_argument('--id', '-id',default='sp')
+	parser.add_argument('--id2', '-id2',default='')
+	parser.add_argument('--ranking', '-r', action='store_true', help='use ranking for RefGTA')
+
+	# running settings
+	parser.add_argument('--batch_size', type=int, default=32)
+	parser.add_argument('--gpu_id', '-g', type=int, default=0)
+	
+	## test setting
+	parser.add_argument('--split', '-split',default='testA')
+	parser.add_argument('--mode','-mode',type=int, default=1, help='0: speaker, 1: reinforcer, 2: ensemble')
+	parser.add_argument('--lamda','-lam',type=float, default=1)
+	parser.add_argument('--beam_width','-beam',type=int, default=10)
+	parser.add_argument('--write_result', default=0, type=int)
+	args = parser.parse_args()
+	return args
